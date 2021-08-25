3ab9491242f2f442036511e7074f91c8ca5f0ebc
hossam.alissa <hossam.alissa@yahoo.com>
2021-08-25 15:33:54 +0300

    EDT singleTab

--- lib/single_tab.dart
+++ lib/single_tab.dart
@@ -6,14 +6,12 @@ import 'package:file_picker/file_picker.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
-import 'package:flutter_vlc_player/flutter_vlc_player.dart';
 import 'package:path_provider/path_provider.dart';
 import 'package:flutter/services.dart' show rootBundle;
 import 'package:provider/provider.dart';
 
 import 'models/models.dart';
-import 'settings/setting.dart';
-import 'vlc_player_with_controls.dart';
+import 'screens/screens.dart';
 
 class SingleTab extends StatefulWidget {
   @override
@@ -33,12 +31,9 @@ class _SingleTabState extends State<SingleTab> {
     }
     final _myFile = File(filePath);
     final List<String> listData = await _myFile.readAsLines(encoding: utf8);
-    Provider.of<Channels>(contextProvider!, listen: false).addChannels(listData);
+    Provider.of<Channels>(context, listen: false).addChannels(listData);
   }
 
-  VlcPlayerController? _controller;
-  final _key = GlobalKey<VlcPlayerWithControlsState>();
-  int? selectedVideoIndex;
 
   Future<File> _loadVideoToFs() async {
     final videoData = await rootBundle.load('assets/sample.mp4');
@@ -47,60 +42,6 @@ class _SingleTabState extends State<SingleTab> {
     var temp = File('$dir/temp.file');
     temp.writeAsBytesSync(videoBytes);
     return temp;
-  }
-
-  x() {
-    selectedVideoIndex = 0;
-    var initVideo = Provider.of<Channels>(contextProvider!, listen: false)
-        .allChannels![selectedVideoIndex!.toInt()];
-    switch (initVideo.type!) {
-      case VideoType.network:
-        _controller = VlcPlayerController.network(
-          initVideo.path.toString(),
-          hwAcc: HwAcc.FULL,
-          options: VlcPlayerOptions(
-            advanced: VlcAdvancedOptions([
-              VlcAdvancedOptions.networkCaching(2000),
-            ]),
-            subtitle: VlcSubtitleOptions([
-              VlcSubtitleOptions.boldStyle(true),
-              VlcSubtitleOptions.fontSize(30),
-              VlcSubtitleOptions.outlineColor(VlcSubtitleColor.yellow),
-              VlcSubtitleOptions.outlineThickness(VlcSubtitleThickness.normal),
-              // works only on externally added subtitles
-              VlcSubtitleOptions.color(VlcSubtitleColor.navy),
-            ]),
-            rtp: VlcRtpOptions([
-              VlcRtpOptions.rtpOverRtsp(true),
-            ]),
-          ),
-        );
-        break;
-      case VideoType.file:
-        var file = File(initVideo.path.toString());
-        _controller = VlcPlayerController.file(
-          file,
-        );
-        break;
-      case VideoType.asset:
-        _controller = VlcPlayerController.asset(
-          initVideo.path.toString(),
-          options: VlcPlayerOptions(),
-        );
-        break;
-    }
-    _controller!.addOnInitListener(() async {
-      await _controller!.startRendererScanning();
-    });
-    _controller!.addOnRendererEventListener((type, id, name) {
-      print('OnRendererEventListener $type $id $name');
-    });
-  }
-
-  @override
-  void initState() {
-    super.initState();
-    x();
   }
 
   @override
@@ -124,25 +65,19 @@ class _SingleTabState extends State<SingleTab> {
             ],
           ),
         ),
-        SizedBox(
-          height: 50.0,
-        ),
-        Container(
-          height: 400,
-          child: VlcPlayerWithControls(key: _key, controller: _controller!),
-        ),
         ListView.builder(
           shrinkWrap: true,
-          itemCount: Provider.of<Channels>(contextProvider!, listen: true)
+          itemCount: Provider.of<Channels>(context, listen: true)
               .allChannels!
               .length
               .toInt(),
           physics: NeverScrollableScrollPhysics(),
           itemBuilder: (BuildContext context, int index) {
-            Channel video = Provider.of<Channels>(contextProvider!, listen: true).allChannels![index];
+            Channel video = Provider.of<Channels>(context, listen: true)
+                .allChannels![index];
 
             return ListTile(
-              selected: Provider.of<Channels>(contextProvider!, listen: true)
+              selected: Provider.of<Channels>(context, listen: true)
                       .indexChannelPlayNow ==
                   index,
               selectedTileColor: Colors.black54,
@@ -150,7 +85,7 @@ class _SingleTabState extends State<SingleTab> {
                 video.tvgName.toString(),
                 overflow: TextOverflow.ellipsis,
                 style: TextStyle(
-                  color: Provider.of<Channels>(contextProvider!, listen: true)
+                  color: Provider.of<Channels>(context, listen: true)
                               .indexChannelPlayNow ==
                           index
                       ? Colors.white
@@ -161,7 +96,7 @@ class _SingleTabState extends State<SingleTab> {
                 video.path.toString(),
                 overflow: TextOverflow.ellipsis,
                 style: TextStyle(
-                  color: Provider.of<Channels>(contextProvider!, listen: true)
+                  color: Provider.of<Channels>(context, listen: true)
                               .indexChannelPlayNow ==
                           index
                       ? Colors.white
@@ -169,29 +104,35 @@ class _SingleTabState extends State<SingleTab> {
                 ),
               ),
               onTap: () async {
-                    await _controller!.setMediaFromNetwork(
-                      video.path.toString(),
-                      hwAcc: HwAcc.FULL,
-                    );
-                //   selectedVideoIndex = index;
-                Provider.of<Channels>(contextProvider!, listen: false).indexChannelPlayNow = index;
-                setState(() {
-                  print(Provider.of<Channels>(contextProvider!, listen: false)
-                      .indexChannelPlayNow);
-                  print(index);
-                });
+                try{
+                  print("===== Start Channel info =====");
+                  print("=${video.tvgId}=${video.tvgName}=${video.tvgCountry}=${video.tvgLanguage}=${video.tvgLogo}=${video.country}");
+                  print("===== End Channel info =====");
+
+                  Provider.of<Channels>(context, listen: false)
+                      .indexChannelPlayNow = index;
+
+                  Navigator.push(
+                      context,
+                      MaterialPageRoute(
+                          builder: (BuildContext context) => FullScreen(path: video.path.toString(),)));
+                  print("===== Done Single Tap =====");
+                }catch(error){
+                  print("===== error Single Tab =====");
+                  print(error);
+                }
+
+
+                // setState(() {
+                //   print(Provider.of<Channels>(context, listen: false)
+                //       .indexChannelPlayNow);
+                //   print(index);
+                // });
               },
             );
           },
         ),
       ],
     );
-  }
-
-  @override
-  void dispose() async {
-    super.dispose();
-    await _controller!.stopRendererScanning();
-    await _controller!.dispose();
   }
 }
