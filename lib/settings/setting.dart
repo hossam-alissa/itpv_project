4e16f985f838f8af5a6eba733d11e1ed000c3244
hossam.alissa <hossam.alissa@yahoo.com>
2021-08-25 15:36:10 +0300

    EDt setting.dart

---
+++ lib/settings/setting.dart
@@ -0,0 +1,7 @@
+import 'package:flutter/material.dart';
+
+const Color mainColor =  Color.fromRGBO(51, 25, 81, 1);
+const Color secondColor =  Color.fromRGBO(51, 25, 81, 1);
+const Color whiteColor = Colors.white;
+
+BuildContext? contextProvider;
