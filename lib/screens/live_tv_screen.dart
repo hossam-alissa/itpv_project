f82db00a9c32c8c7deb520e6c08e095e066eb2c2
hossam.alissa <hossam.alissa@yahoo.com>
2021-08-26 14:09:36 +0300

    NEW liveTvScreen

---
+++ lib/screens/live_tv_screen.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+
+class LiveTvScreen extends StatefulWidget {
+  const LiveTvScreen({Key? key}) : super(key: key);
+
+  @override
+  _LiveTvScreenState createState() => _LiveTvScreenState();
+}
+
+class _LiveTvScreenState extends State<LiveTvScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      color: Colors.yellow,
+    );
+  }
+}
