600ce455390819a4c4b2caab3dde50c2234ad8ae
hossam.alissa <hossam.alissa@yahoo.com>
2021-08-26 10:05:47 +0300

    ADD theme setting

--- lib/settings/setting.dart
+++ lib/settings/setting.dart
@@ -1,7 +1,25 @@
 import 'package:flutter/material.dart';
 
-const Color mainColor =  Color.fromRGBO(51, 25, 81, 1);
-const Color secondColor =  Color.fromRGBO(51, 25, 81, 1);
+//App Colors
+int themeNumber = 0;
+const List<Color> mainColor =  [Color.fromRGBO(51, 25, 81, 1),Color.fromRGBO(143, 59, 186, 1)];
+const List<Color> secondColor =  [Color.fromRGBO(143, 59, 186, 1),Color.fromRGBO(143, 59, 186, 1)];
 const Color whiteColor = Colors.white;
 
+//Font size for App
+double fontSize = 20;
+double fontSizeTitle = 25;
+
+//Context Provider for App
 BuildContext? contextProvider;
+
+//Customize font size for screen size
+void appSetting(double screenSize){
+  if(screenSize > 800){
+
+  }else if(screenSize >= 800 && screenSize <1200 ){
+
+  }else{//screenSize >= 1200
+
+  }
+}
