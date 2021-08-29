2d16000f53729ac980635ca0e939a650ffc7502e
hossam.alissa <hossam.alissa@yahoo.com>
2021-08-29 10:53:58 +0300

    DET file picakr

--- lib/main.dart
+++ lib/main.dart
@@ -19,7 +19,7 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
-      title: 'Flutter Demo',
+      title: 'MX IP TV',
       theme: ThemeData(
         primarySwatch: Colors.blue,
       ),
